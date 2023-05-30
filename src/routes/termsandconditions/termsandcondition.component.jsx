2b286281c59a8e886eb9befc738040b0603ef2f4
David Etuk <djetuk@gmail.com>
2023-05-30 19:18:39 +0100

    feat: developed the terms and conditions page

---
+++ src/routes/termsandconditions/termsandcondition.component.jsx
@@ -0,0 +1,370 @@
+const TermsAndConditions = () => {
+  return (
+    <div className="px-44 leading-normal text-medium">
+      <div className="mb-8">
+        <h2 className="text-7xl font-bold leading-tight font-heading">
+          Terms & Conditions
+        </h2>
+      </div>
+
+      <div className="text-lg pr-16 ">
+        <h3 className="text-3xl font-bold py-4">Welcome to Digitalesque!</h3>
+        <p className="py-3">
+          These terms and conditions outline the rules and regulations for the
+          use of Digitalesque Limited's Website, located at&nbsp;
+          <a className="underline" href="https://digitalesque.xyz">
+            https://digitalesque.xyz
+          </a>
+          .
+        </p>
+        <p className="py-3">
+          By accessing this website we assume you accept these terms and
+          conditions. Do not continue to use Digitalesque if you do not agree to
+          take all of the terms and conditions stated on this page.
+        </p>
+        <p className="py-3">
+          The following terminology applies to these Terms and Conditions,
+          Privacy Statement and Disclaimer Notice and all Agreements: "Client",
+          "You" and "Your" refers to you, the person log on this website and
+          compliant to the Company’s terms and conditions. "The Company",
+          "Ourselves", "We", "Our" and "Us", refers to our Company. "Party",
+          "Parties", or "Us", refers to both the Client and ourselves. All terms
+          refer to the offer, acceptance and consideration of payment necessary
+          to undertake the process of our assistance to the Client in the most
+          appropriate manner for the express purpose of meeting the Client’s
+          needs in respect of provision of the Company’s stated services, in
+          accordance with and subject to, prevailing law of Netherlands. Any use
+          of the above terminology or other words in the singular, plural,
+          capitalization and/or he/she or they, are taken as interchangeable and
+          therefore as referring to same.
+        </p>
+      </div>
+
+      <div className="pt-8 pr-16">
+        <h3 className="text-3xl font-bold py-4">Cookies</h3>
+        <p className="py-3">
+          We employ the use of cookies. By accessing Digitalesque, you agreed to
+          use cookies in agreement with the Digitalesque Limited's Privacy
+          Policy.
+        </p>
+        <p className="py-3">
+          We employ the use of cookies. By accessing Digitalesque, you agreed to
+          use cookies in agreement with the Digitalesque Limited's Privacy
+          Policy.
+        </p>
+      </div>
+
+      <div className="pt-8 pr-16 ">
+        <h3 className="text-3xl font-bold py-4">License</h3>
+        <p className="py-3">
+          Unless otherwise stated, Digitalesque Limited and/or its licensors own
+          the intellectual property rights for all material on Digitalesque. All
+          intellectual property rights are reserved. You may access this from
+          Digitalesque for your own personal use subjected to restrictions set
+          in these terms and conditions.
+        </p>
+        <p className="py-3">You must not:</p>
+        <ul className="list-disc text-gray-300 px-6">
+          <li className="p-2">
+            <p className="text-black">Republish material from Digitalesque</p>
+          </li>
+          <li className="p-2">
+            <p className="text-black">
+              Sell, rent or sub-license material from Digitalesque
+            </p>
+          </li>
+          <li className="p-2">
+            <p className="text-black">
+              Reproduce, duplicate or copy material from Digitalesque
+            </p>
+          </li>
+          <li className="p-2">
+            <p className="text-black">Redistribute content from Digitalesque</p>
+          </li>
+        </ul>
+        <p className="py-4">This Agreement shall begin on the date hereof.</p>
+        <p className="py-3">
+          Parts of this website offer an opportunity for users to post and
+          exchange opinions and information in certain areas of the website.
+          Digitalesque Limited does not filter, edit, publish or review Comments
+          prior to their presence on the website. Comments do not reflect the
+          views and opinions of Digitalesque Limited,its agents and/or
+          affiliates. Comments reflect the views and opinions of the person who
+          post their views and opinions. To the extent permitted by applicable
+          laws, Digitalesque Limited shall not be liable for the Comments or for
+          any liability, damages or expenses caused and/or suffered as a result
+          of any use of and/or posting of and/or appearance of the Comments on
+          this website.
+        </p>
+        <p className="py-3">
+          Digitalesque Limited reserves the right to monitor all Comments and to
+          remove any Comments which can be considered inappropriate, offensive
+          or causes breach of these Terms and Conditions.
+        </p>
+        <p className="py-3">You warrant and represent that:</p>
+        <ul className="list-disc text-gray-300 px-10">
+          <li className="p-2">
+            <p className="text-black">
+              You are entitled to post the Comments on our website and have all
+              necessary licenses and consents to do so;
+            </p>
+          </li>
+          <li className="p-2">
+            <p className="text-black">
+              The Comments do not invade any intellectual property right,
+              including without limitation copyright, patent or trademark of any
+              third party;
+            </p>
+          </li>
+          <li className="p-2">
+            <p className="text-black">
+              The Comments do not contain any defamatory, libelous, offensive,
+              indecent or otherwise unlawful material which is an invasion of
+              privacy
+            </p>
+          </li>
+          <li className="p-2">
+            <p className="text-black">
+              The Comments will not be used to solicit or promote business or
+              custom or present commercial activities or unlawful activity.
+            </p>
+          </li>
+        </ul>
+        <p className="py-4">
+          You hereby grant Digitalesque Limited a non-exclusive license to use,
+          reproduce, edit and authorize others to use, reproduce and edit any of
+          your Comments in any and all forms, formats or media.
+        </p>
+      </div>
+
+      <div className="pt-8 pr-16 ">
+        <h3 className="text-3xl font-bold py-4">Hyperlinking to our Content</h3>
+        <p className="py-3">
+          The following organizations may link to our Website without prior
+          written approval:
+        </p>
+        <ul className="list-disc text-gray-300 px-10">
+          <li className="p-2">
+            <p className="text-black">Government agencies;</p>
+          </li>
+          <li className="p-2">
+            <p className="text-black">Search engines;</p>
+          </li>
+          <li className="p-2">
+            <p className="text-black">News organizations;</p>
+          </li>
+          <li className="p-2">
+            <p className="text-black">
+              Online directory distributors may link to our Website in the same
+              manner as they hyperlink to the Websites of other listed
+              businesses; and
+            </p>
+          </li>
+          <li className="p-2">
+            <p className="text-black">
+              System wide Accredited Businesses except soliciting non-profit
+              organizations, charity shopping malls, and charity fundraising
+              groups which may not hyperlink to our Web site.
+            </p>
+          </li>
+        </ul>
+        <p className="py-4">
+          These organizations may link to our home page, to publications or to
+          other Website information so long as the link: (a) is not in any way
+          deceptive; (b) does not falsely imply sponsorship, endorsement or
+          approval of the linking party and its products and/or services; and
+          (c) fits within the context of the linking party’s site.
+        </p>
+        <p className="py-4">
+          We may consider and approve other link requests from the following
+          types of organizations:
+        </p>
+        <ul className="list-disc text-gray-300 px-10">
+          <li className="p-2">
+            <p className="text-black">
+              commonly-known consumer and/or business information sources;
+            </p>
+          </li>
+          <li className="p-2">
+            <p className="text-black">dot.com community sites;</p>
+          </li>
+          <li className="p-2">
+            <p className="text-black">
+              associations or other groups representing charities;
+            </p>
+          </li>
+          <li className="p-2">
+            <p className="text-black">online directory distributors;</p>
+          </li>
+          <li className="p-2">
+            <p className="text-black">internet portals;</p>
+          </li>
+          <li className="p-2">
+            <p className="text-black">
+              accounting, law and consulting firms; and
+            </p>
+          </li>
+          <li className="p-2">
+            <p className="text-black">
+              educational institutions and trade associations.
+            </p>
+          </li>
+        </ul>
+        <p className="py-4">
+          We will approve link requests from these organizations if we decide
+          that: (a) the link would not make us look unfavorably to ourselves or
+          to our accredited businesses; (b) the organization does not have any
+          negative records with us; (c) the benefit to us from the visibility of
+          the hyperlink compensates the absence of Digitalesque Limited; and (d)
+          the link is in the context of general resource information.
+        </p>
+        <p className="py-3">
+          These organizations may link to our home page so long as the link: (a)
+          is not in any way deceptive; (b) does not falsely imply sponsorship,
+          endorsement or approval of the linking party and its products or
+          services; and (c) fits within the context of the linking party’s site.
+        </p>
+        <p className="py-3">
+          If you are one of the organizations listed in paragraph 2 above and
+          are interested in linking to our website, you must inform us by
+          sending an e-mail to Digitalesque Limited. Please include your name,
+          your organization name, contact information as well as the URL of your
+          site, a list of any URLs from which you intend to link to our Website,
+          and a list of the URLs on our site to which you would like to link.
+          Wait 2-3 weeks for a response.
+        </p>
+        <p className="py-4">
+          Approved organizations may hyperlink to our Website as follows:
+        </p>
+        <ul className="list-disc text-gray-300 px-10">
+          <li className="p-2">
+            <p className="text-black">By use of our corporate name; or</p>
+          </li>
+          <li className="p-2">
+            <p className="text-black">
+              By use of the uniform resource locator being linked to; or
+            </p>
+          </li>
+          <li className="p-2">
+            <p className="text-black">
+              By use of any other description of our Website being linked to
+              that makes sense within the context and format of content on the
+              linking party’s site.
+            </p>
+          </li>
+        </ul>
+        <p className="py-4">
+          No use of Digitalesque Limited's logo or other artwork will be allowed
+          for linking absent a trademark license agreement.
+        </p>
+      </div>
+
+      <div className="pt-8 pr-16 ">
+        <h3 className="text-3xl font-bold py-4">iFrames</h3>
+        <p className="py-2">
+          Without prior approval and written permission, you may not create
+          frames around our Webpages that alter in any way the visual
+          presentation or appearance of our Website.
+        </p>
+      </div>
+
+      <div className="pt-8 pr-16 ">
+        <h3 className="text-3xl font-bold py-4">Content Liability</h3>
+        <p className="py-2">
+          We shall not be hold responsible for any content that appears on your
+          Website. You agree to protect and defend us against all claims that is
+          rising on your Website. No link(s) should appear on any Website that
+          may be interpreted as libelous, obscene or criminal, or which
+          infringes, otherwise violates, or advocates the infringement or other
+          violation of, any third party rights.
+        </p>
+      </div>
+
+      <div className="pt-8 pr-16 ">
+        <h3 className="text-3xl font-bold py-4">Your Privacy</h3>
+        <a className="underline" href="/privacy">
+          Please read Privacy Policy
+        </a>
+      </div>
+
+      <div className="pt-8 pr-16 ">
+        <h3 className="text-3xl font-bold py-4">Reservation of Rights</h3>
+        <p className="py-2">
+          We reserve the right to request that you remove all links or any
+          particular link to our Website. You approve to immediately remove all
+          links to our Website upon request. We also reserve the right to amen
+          these terms and conditions and it’s linking policy at any time. By
+          continuously linking to our Website, you agree to be bound to and
+          follow these linking terms and conditions.
+        </p>
+      </div>
+
+      <div className="pt-8 pr-16 ">
+        <h3 className="text-3xl font-bold py-4">
+          Removal of links from our website
+        </h3>
+        <p className="py-2">
+          If you find any link on our Website that is offensive for any reason,
+          you are free to contact and inform us any moment. We will consider
+          requests to remove links but we are not obligated to or so or to
+          respond to you directly.
+        </p>
+        <p className="py-3">
+          If you find any link on our Website that is offensive for any reason,
+          you are free to contact and inform us any moment. We will consider
+          requests to remove links but we are not obligated to or so or to
+          respond to you directly.
+        </p>
+      </div>
+
+      <div className="pt-8 pr-16 ">
+        <h3 className="text-3xl font-bold py-4">Disclaimer</h3>
+        <p className="py-2">
+          To the maximum extent permitted by applicable law, we exclude all
+          representations, warranties and conditions relating to our website and
+          the use of this website. Nothing in this disclaimer will:
+        </p>
+        <ul className="list-disc text-gray-300 px-10">
+          <li className="p-2">
+            <p className="text-black">
+              limit or exclude our or your liability for death or personal
+              injury;
+            </p>
+          </li>
+          <li className="p-2">
+            <p className="text-black">
+              limit or exclude our or your liability for fraud or fraudulent
+              misrepresentation;
+            </p>
+          </li>
+          <li className="p-2">
+            <p className="text-black">
+              limit any of our or your liabilities in any way that is not
+              permitted under applicable law; or
+            </p>
+          </li>
+          <li className="p-2">
+            <p className="text-black">
+              exclude any of our or your liabilities that may not be excluded
+              under applicable law.
+            </p>
+          </li>
+        </ul>
+        <p className="py-4">
+          The limitations and prohibitions of liability set in this Section and
+          elsewhere in this disclaimer: (a) are subject to the preceding
+          paragraph; and (b) govern all liabilities arising under the
+          disclaimer, including liabilities arising in contract, in tort and for
+          breach of statutory duty.
+        </p>
+        <p className="py-6">
+          As long as the website and the information and services on the website
+          are provided free of charge, we will not be liable for any loss or
+          damage of any nature.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default TermsAndConditions;
