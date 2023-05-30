c9154c9895c2b7ce512f163b7cb77ccab44c4861
David Etuk <djetuk@gmail.com>
2023-05-30 19:50:51 +0100

    feat: added the carousel and overview component

---
+++ src/components/overview-preview/overview.preview.component.jsx
@@ -0,0 +1,13 @@
+import Carousel from "../carousel/carousel.component";
+import Overview from "../overview/overview.component";
+
+const OverviewPreview = () => {
+  return (
+    <div>
+      <Carousel />
+      <Overview />
+    </div>
+  );
+};
+
+export default OverviewPreview;
