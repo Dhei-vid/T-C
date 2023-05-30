686a831fd0866ea2261fa9caa3609f1b364b9b38
David Etuk <djetuk@gmail.com>
2023-05-30 18:33:51 +0100

    fix: sorted nested routing by adding / for absolute routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+
+import Navigation from "./components/navigation/navigation.component";
+import Home from "./routes/home/home.components";
+import OverviewPreview from "./components/overview-preview/overview.preview.component";
+import Capabilities from "./routes/capabilities/capabilties.component";
+import Insight from "./routes/insight/insight.component";
+import Contact from "./components/contact/contact.component";
+import Footer from "./components/footer/footer.component";
+import Client from "./routes/clients/clients.component";
+import TermsAndConditions from "./routes/terms&conditions/terms&condition.component";
+import Privacy from "./routes/privacy/privacy.component";
+
+import AfricaDigital from "./routes/insight/africa-digital.component";
+import LessonsFromIndia from "./routes/insight/lessons-from-india.component";
+import PromisesPeril from "./routes/insight/promises-peril.component";
+import JackOfAllTrades from "./routes/insight/jack-of-all-trades.component";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route path="/" element={<Footer />}>
+          <Route path="/" element={<Contact />}>
+            <Route index element={<Home />} />
+            <Route path="overview" element={<OverviewPreview />} />
+            <Route path="capabilities" element={<Capabilities />} />
+            <Route path="clients" element={<Client />} />
+            <Route path="terms" element={<TermsAndConditions />} />
+            <Route path="privacy" element={<Privacy />} />
+            <Route path="/insights" element={<Insight />} />
+            <Route
+              path="/insights/africa-digital"
+              element={<AfricaDigital />}
+            />
+            <Route
+              path="/insights/lessons-from-india"
+              element={<LessonsFromIndia />}
+            />
+            <Route path="/insights/promises" element={<PromisesPeril />} />
+            <Route
+              path="/insights/jack-of-all-trades"
+              element={<JackOfAllTrades />}
+            />
+          </Route>
+        </Route>
+      </Route>
+    </Routes>
   );
 }
 
