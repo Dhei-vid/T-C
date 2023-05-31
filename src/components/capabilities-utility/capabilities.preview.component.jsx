74082f66adb6bfb313d91f41c2465ff09eb66a02
David Etuk <djetuk@gmail.com>
2023-05-31 17:39:24 +0100

    feat: added responsive styles

--- src/components/capabilities-utility/capabilities.preview.component.jsx
+++ src/components/capabilities-utility/capabilities.preview.component.jsx
@@ -3,10 +3,10 @@ import CapabilitiesGridImages from "./capabilities.grid.component";
 
 const CapabiltiesPreview = () => {
   return (
-    <div className="grid grid-cols-3 ">
-      <div className="col-span-3 pl-44 py-12">
+    <div className="grid md:grid-cols-3 grid-cols-1">
+      <div className="md:col-span-3 md:py-20 pl-10 py-9 md:pt-32">
         <Reveal delay={0.4} y1={-20} y2={0}>
-          <h3 className="text-3xl font-bold tracking-wider">
+          <h3 className="md:text-3xl font-bold tracking-wider">
             Our Capabilities
           </h3>
         </Reveal>
@@ -14,7 +14,7 @@ const CapabiltiesPreview = () => {
 
       <CapabilitiesGridImages />
 
-      <div className="col-end-4 underline hover:text-gray-500 text-lg md:text-xl py-10 px-6">
+      <div className="md:col-end-4 underline justify-self-end hover:text-gray-500 text-lg md:text-xl py-10 md:pr-[40%] pr-6">
         <a className="text-lg md:text-xl" href="/capabilities">
           All Capabilities
         </a>
