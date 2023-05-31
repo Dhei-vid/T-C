018a8677b461477d3aa555434973540874bd67a7
David Etuk <djetuk@gmail.com>
2023-05-31 17:57:54 +0100

    feat: added responsive features

--- src/components/contact/contact.component.jsx
+++ src/components/contact/contact.component.jsx
@@ -5,10 +5,10 @@ const Contact = () => {
   return (
     <>
       <Outlet />
-      <div className="px-36 py-28">
+      <div className="md:px-36 py-28 px-6">
         <Reveal delay={0.5} y1={75} y2={0}>
-          <div className="pb-14 px-9">
-            <h3 className="font-bold text-4xl leading-tight tracking-wide">
+          <div className="pb-14">
+            <h3 className="font-bold md:text-4xl text-2xl leading-tight tracking-wide">
               If you’re interested in working with us and want to find out more
               about the results we’ve achieved, we’d love to hear from you.
             </h3>
@@ -16,7 +16,7 @@ const Contact = () => {
         </Reveal>
         <div className="px-4 pt-6 md:pl-[48vw] border-t-2 border-black pt-8"></div>
         <Reveal>
-          <div className="grid px-4 pt-6 md:pl-[48vw] text-xl">
+          <div className="grid px-4 pt-6 md:pl-[65%] pl-[45%] md:text-xl text-base">
             <h4 className="">Can we do business?</h4>
             <a
               className="underline cursor-pointer hover:text-gray-500"
