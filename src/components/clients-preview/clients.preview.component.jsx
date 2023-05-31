5acd04164847ce84cda108698398b5d0482aaa5a
David Etuk <djetuk@gmail.com>
2023-05-31 17:57:11 +0100

    feat: added responsive features

--- src/components/clients-preview/clients.preview.component.jsx
+++ src/components/clients-preview/clients.preview.component.jsx
@@ -2,10 +2,10 @@ import Reveal from "../../utility/reveal";
 
 const ClientsPreview = () => {
   return (
-    <div className="px-44">
+    <div className="md:px-44">
       <Reveal delay={0.5} y1={-75} y2={0}>
         <div className="pb-14 pt-20">
-          <h3 className="font-bold text-4xl leading-tight tracking-wide px-6">
+          <h3 className="font-bold md:text-4xl text-[1.5rem] leading-tight tracking-wide px-6">
             We have worked closely and successfully with government departments
             and major companies, and pride ourselves on meeting and exceeding
             our client’s high expectations. Here, you can find a selection of
--- src/components/clients-preview/clients.preview.grid.component.jsx
+++ src/components/clients-preview/clients.preview.grid.component.jsx
@@ -7,7 +7,7 @@ const ClientGrid = ({ data }) => {
             key={index}
             className="col-end-3 py-10 px-4 border-t-2 border-black first:border-none"
           >
-            <h1 className="text-xl md:text-3xl font-heading tracking-wide font-bold">
+            <h1 className="text-xl md:text-3xl text-2xl font-heading tracking-wide font-bold">
               {item.header}
             </h1>
             <p className="md:text-lg text-black">{item.text}</p>
