8986682afd9cb64be1771fe618a78524d14d7486
David Etuk <djetuk@gmail.com>
2023-05-31 18:00:06 +0100

    feat: added responsive features

--- src/components/overview/overview.component.jsx
+++ src/components/overview/overview.component.jsx
@@ -2,11 +2,11 @@ import Reveal from "../../utility/reveal";
 
 const Overview = () => {
   return (
-    <div className="px-44">
-      <section className="pt-10 pr-10">
+    <div className="md:px-36 px-4">
+      <section className="pt-10 md:pr-10 px-4">
         <Reveal delay={0.6} y1={75} y2={0}>
           <div>
-            <h1 className="leading-tight font-extrabold text-4xl tracking-wide">
+            <h1 className="leading-tight font-extrabold md:text-4xl text-[1.5rem] tracking-wide">
               Our blueprint for digital transformation below gives an overview
               of the sequence, checklist, and guides at all levels of the
               organization through the journey ensuring that no area of the
@@ -16,12 +16,12 @@ const Overview = () => {
         </Reveal>
       </section>
 
-      <section className="grid grid-cols-2 gap-x-24 pt-12">
+      <section className="grid md:grid-cols-2 gap-x-24 pt-12">
         <div className=" border-t-2 border-black px-8 py-10">
-          <h2 className="text-4xl mb-4 tracking-wide font-extrabold ">
+          <h2 className="md:text-4xl text-[1.5rem] mb-4 tracking-wide font-extrabold ">
             Technology
           </h2>
-          <p className="max-w-md text-xl">
+          <p className="max-w-md md:text-xl">
             Digital transformation involves using digital technologies to remake
             a process to become more efficient or effective. The idea is to use
             technology not just to replicate an existing service in a digital
@@ -31,8 +31,10 @@ const Overview = () => {
         </div>
 
         <div className="border-t-2 border-black px-8 py-10">
-          <h2 className="text-4xl mb-4 tracking-wide font-extrabold  ">Data</h2>
-          <p className="max-w-md text-xl">
+          <h2 className="md:text-4xl text-[1.5rem] mb-4 tracking-wide font-extrabold  ">
+            Data
+          </h2>
+          <p className="max-w-md md:text-xl">
             Big Data is at the heart of digital transformation because it brings
             all the points together in one place for a more comprehensive and
             holistic view of the entire organization's ecosystem. Big data has
@@ -43,10 +45,10 @@ const Overview = () => {
         </div>
 
         <div className="border-t-2 border-black px-8 py-10">
-          <h2 className="text-4xl mb-4 tracking-wide font-extrabold">
+          <h2 className="md:text-4xl text-[1.5rem] mb-4 tracking-wide font-extrabold">
             Process
           </h2>
-          <p className="max-w-md text-xl">
+          <p className="max-w-md md:text-xl">
             Digital transformation requires an end-to-end mindset, a rethinking
             of ways to meet customer needs, seamless connection of work
             activities, and the ability to manage across silos going forward.
@@ -56,10 +58,10 @@ const Overview = () => {
         </div>
 
         <div className="border-t-2 border-black px-8 py-10">
-          <h2 className="text-4xl mb-4 tracking-wide font-extrabold  ">
+          <h2 className="md:text-4xl text-[1.5rem] mb-4 tracking-wide font-extrabold  ">
             Culture
           </h2>
-          <p className="max-w-md text-xl">
+          <p className="max-w-md md:text-xl">
             The core driver of digital transformation is a digital culture
             because it empowers people to deliver results faster by increasing
             adoption, creating the right organizational alignment needed for the
