a794c397b0a8d973575bed9747d4c040219d634e
David Etuk <djetuk@gmail.com>
2023-06-01 13:40:38 +0100

    fix: revised responsive styles
    fix: adjusted screen resolution

--- src/routes/clients/clients.component.jsx
+++ src/routes/clients/clients.component.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 import Carousel from "../../components/carousel/carousel.component";
 import ClientsPreview from "../../components/clients-preview/clients.preview.component";
 import ClientGrid from "../../components/clients-preview/clients.preview.grid.component";
@@ -35,10 +37,31 @@ const clientData = [
 ];
 
 const Client = () => {
+  const [dimensions, setDimensions] = useState(
+    {
+      height: window.innerHeight,
+      width: window.innerWidth,
+    },
+    []
+  );
+
+  console.log(dimensions.width);
+
+  useEffect(() => {
+    const handleScreenWidth = () => {
+      setDimensions({
+        height: window.innerHeight,
+        width: window.innerWidth,
+      });
+    };
+
+    window.addEventListener("resize", handleScreenWidth);
+  }, []);
+
   return (
     <>
-      <div className="px-44">
-        <h1 className="text-7xl font-bold leading-none tracking-wider">
+      <div className="md:px-44 px-6">
+        <h1 className="md:text-7xl text-5xl font-bold leading-none tracking-wider">
           The Clientele
         </h1>
       </div>
@@ -48,17 +71,17 @@ const Client = () => {
 
       {/* CLIENT PREVIEW */}
       <ClientsPreview />
-      <div className="px-44">
-        <div className="grid grid-cols-2 gap-x-24 border-t-2 border-black pt-8">
+      <div className="md:px-44 px-6">
+        <div className="grid md:grid-cols-2 md:gap-x-24 gap-x-2 border-t-2 border-black pt-8">
           <ClientGrid data={clientData} />
         </div>
       </div>
 
       {/* INSIGHT PREVIEW */}
-      <div className="col-span-4 px-6 pt-24 pl-44">
+      <div className="col-span-4 px-6 pt-24 md:pl-44">
         <h3 className="text-3xl font-bold tracking-wider">Our Insights</h3>
       </div>
-      <InsightPreview colsNo={4} />
+      <InsightPreview colsNo={dimensions.width >= 700 ? 4 : 1} />
     </>
   );
 };
