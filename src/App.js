e0b28fee891ad80bf419a60bf5a29d6601ad966a
David Etuk <djetuk@gmail.com>
2023-06-01 13:20:07 +0100

    feat: added blueprint, contact and about routes

--- src/App.js
+++ src/App.js
@@ -5,31 +5,33 @@ import Home from "./routes/home/home.components";
 import OverviewPreview from "./components/overview-preview/overview.preview.component";
 import Capabilities from "./routes/capabilities/capabilties.component";
 import Insight from "./routes/insight/insight.component";
-import Contact from "./components/contact/contact.component";
+import ContactPreview from "./components/contact-preview/contact.preview.component";
 import Footer from "./components/footer/footer.component";
 import Client from "./routes/clients/clients.component";
 import TermsAndConditions from "./routes/termsandconditions/termsandcondition.component";
 import Privacy from "./routes/privacy/privacy.component";
+import About from "./routes/about/about.component";
 
 import AfricaDigital from "./routes/insight/africa-digital.component";
 import LessonsFromIndia from "./routes/insight/lessons-from-india.component";
 import PromisesPeril from "./routes/insight/promises-peril.component";
 import JackOfAllTrades from "./routes/insight/jack-of-all-trades.component";
-
-import "./App.css";
+import Blueprint from "./routes/blueprint/blueprint.component";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route path="/" element={<Footer />}>
-          <Route path="/" element={<Contact />}>
+          <Route path="/" element={<ContactPreview />}>
             <Route index element={<Home />} />
             <Route path="overview" element={<OverviewPreview />} />
             <Route path="capabilities" element={<Capabilities />} />
             <Route path="clients" element={<Client />} />
             <Route path="terms" element={<TermsAndConditions />} />
             <Route path="privacy" element={<Privacy />} />
+            <Route path="about" element={<About />} />
+            <Route path="blueprint" element={<Blueprint />} />
             <Route path="/insights" element={<Insight />} />
             <Route
               path="/insights/africa-digital"
