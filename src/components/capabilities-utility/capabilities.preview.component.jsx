1470f20e0549c43072a47338316deb010947aced
David Etuk <djetuk@gmail.com>
2023-06-02 02:28:37 +0100

    fix: sorting overlay transitioning
    fix: sort transitioning by passing transition margins as props

--- src/components/capabilities-utility/capabilities.preview.component.jsx
+++ src/components/capabilities-utility/capabilities.preview.component.jsx
@@ -6,13 +6,17 @@ const CapabiltiesPreview = () => {
     <div className="grid md:grid-cols-3 grid-cols-1">
       <div className="md:col-span-3 py-10 md:pl-44 py-9 md:pt-32">
         <Reveal delay={0.4} y1={-20} y2={0}>
-          <h3 className="md:text-3xl tracking-wider font-bold">
+          <h3 className={`md:text-3xl tracking-wider font-bold px-6`}>
             Our Capabilities
           </h3>
         </Reveal>
       </div>
 
-      <CapabilitiesGridImages />
+      <CapabilitiesGridImages
+        translate1={"50%"}
+        translate2={"50%"}
+        translate3={"30%"}
+      />
 
       <div className="md:col-end-4 underline justify-self-end hover:text-gray-500 text-lg md:text-xl py-10 md:pr-[40%] pr-6">
         <a className="text-lg md:text-xl" href="/capabilities">
